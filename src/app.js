7359517dd904cefd7663d3016a19875227d60cb5
vonramwel <vjnicolas@up.edu.ph>
2022-10-20 15:48:53 +0000

    feat: add openapi glue and swagger

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
 import Fastify from 'fastify';
 import sensible from '@fastify/sensible';
-import { general } from './services/general/index.js';
-import { createTodo } from './services/todos/create-todo.js';
-import { getManyTodo } from './services/todos/get-many-todo.js';
-import { getTodo } from './services/todos/get-todo.js';
-import { updateTodo } from './services/todos/update-todo.js';
-import { deleteTodo } from './services/todos/delete-todo.js';
+import openAPIGlue from 'fastify-openapi-glue';
+import swagger from '@fastify/swagger';
 
 const prefix = '/api';
 
@@ -14,23 +10,39 @@ export async function build () {
   const fastify = Fastify({ logger: true });
   fastify.register(sensible);
 
-  fastify.get(prefix, general);
+  const openAPIGlueOptions = {
+    // specification,
+    // service,
+    prefix
+  };
+
+  const swaggerOptions = {
+    // openapi: specification,
+    // routePrefix: '/docs',
+    exposeRoute: true
+  };
+
+  fastify.register(swagger, swaggerOptions);
+  fastify.register(openAPIGlue, openAPIGlueOptions);
+
+
+  
 
   // create todo
-  fastify.post(`${prefix}/todo`, createTodo);
+  // fastify.post(`${prefix}/todo`, createTodo);
 
-  // get many todo
-  fastify.get(`${prefix}/todo`, getManyTodo);
+  // // get many todo
+  // fastify.get(`${prefix}/todo`, getManyTodo);
 
-  // get one todo
-  fastify.get(`${prefix}/todo/:todoId`, getTodo);
-  //    this means that it is not a path but a variable /todo/:todoId`
+  // // get one todo
+  // fastify.get(`${prefix}/todo/:todoId`, getTodo);
+  // //    this means that it is not a path but a variable /todo/:todoId`
 
-  // update one todo
-  fastify.put(`${prefix}/todo/:todoId`, updateTodo);
+  // // update one todo
+  // fastify.put(`${prefix}/todo/:todoId`, updateTodo);
 
-  // delete one todo
-  fastify.delete(`${prefix}/todo/:todoId`, deleteTodo);
+  // // delete one todo
+  // fastify.delete(`${prefix}/todo/:todoId`, deleteTodo);
 
   return fastify;
 }
