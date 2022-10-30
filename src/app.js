76799bd5daf20789a193035d1ac166085b9196ad
vonramwel <vjnicolas@up.edu.ph>
2022-10-30 13:52:14 +0000

    refactor: move the code towards specification as well as refactor paths

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import Fastify from 'fastify';
 import sensible from '@fastify/sensible';
 import openAPIGlue from 'fastify-openapi-glue';
 import swagger from '@fastify/swagger';
+import { Service } from './services/index.js';
+import { specification } from './specification/index.js';
 
 const prefix = '/api';
 
@@ -10,15 +12,17 @@ export async function build () {
   const fastify = Fastify({ logger: true });
   fastify.register(sensible);
 
+  const service = new Service();
+
   const openAPIGlueOptions = {
-    // specification,
-    // service,
+    specification,
+    service,
     prefix
   };
 
   const swaggerOptions = {
-    // openapi: specification,
-    // routePrefix: '/docs',
+     openapi: specification,
+     routePrefix: '/docs',
     exposeRoute: true
   };
 
